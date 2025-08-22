6ed16199ba492dfe829cd58b27947133c28886ee
Makenley Luctamar <luctamak001@warren.k12.in.us>
2025-08-22 13:53:12 -0400

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner input = new Scanner(System.in);
+
+
+        int a = 15;
+        int b = 23;
+
+        System.out.println("Max of 15 and 23: " + findMaximum(15, 23));
+
+    }
+
+    private static int findMaximum(int a, int b) {
+        return;
+    }
+}
