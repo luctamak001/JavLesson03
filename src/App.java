d15c3cf6739cf0bcbfba0644f06d295fc75e495f
Makenley Luctamar <luctamak001@warren.k12.in.us>
2025-08-26 14:00:02 -0400

    upd input

--- src/App.java
+++ src/App.java
@@ -5,15 +5,21 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter a value for a: ");
+        int a = input.nextInt();
+        System.out.println("Enter a value for b: ");
+        int b = input.nextInt();
 
-        int a = 15;
-        int b = 23;
 
         System.out.println("Max of 15 and 23: " + findMaximum(15, 23));
+
+
+        System.out.println("Max of " + a + " and " + b + ": " + findMaximum(15, 23));
+
 
     }
 
     private static int findMaximum(int a, int b) {
-        return;
+        return a > b ? a : b;
     }
 }
